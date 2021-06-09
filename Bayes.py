180af28c7410bda4edf23bd20915e8f7406360cd
Nya2233 <45359144+i2w3@users.noreply.github.com>
2021-06-09 16:18:44 +0800

    标准化后的准确率

--- Bayes.py
+++ Bayes.py
@@ -4,6 +4,7 @@ from DataProcess import combineSeqFid, combineSeqData
 from sklearn import model_selection
 from sklearn.naive_bayes import GaussianNB
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 Data = pd.read_csv('pdData.csv')
 X = combineSeqData(Data)
@@ -12,6 +13,16 @@ y = np.array(Data['Face'].values)
 pca = PCA(n_components=76, svd_solver='auto',
           whiten=True).fit(X)
 X1 = pca.transform(X)
+
+sc = StandardScaler()
+sc.fit(X)
+X2 = sc.transform(X)
+
+pca = PCA(n_components=76, svd_solver='auto',
+          whiten=True).fit(X2)
+X3 = pca.transform(X2)
+
+Kmeans = 1
 # 74 0.601884
 # 75 0.607680
 # 76 0.608690
@@ -21,7 +32,7 @@ X1 = pca.transform(X)
 num_folds = 10
 scoring = 'accuracy'
 
-for name, data in (["Raw", X], ["PCA", X1]):
+for name, data in (["Raw", X], ["PCA", X1], ["Std", X2], ["Std+PCA", X3]):
     kfold = model_selection.KFold(n_splits=num_folds)
     cv_results = model_selection.cross_val_score(GaussianNB(), data, y, cv=kfold, scoring=scoring)
     msg = "%s NB: %f (%f)" % (name, cv_results.mean(), cv_results.std())
