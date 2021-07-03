6b41f8a542e92ca9b3b65b1948f6ae58480227e1
Nya2233 <45359144+i2w3@users.noreply.github.com>
2021-07-03 23:38:31 +0800

    Add "pdDataNF.csv"，生成将funny标签转为smiling的数据文件

--- build_pdData.py
+++ build_pdData.py
@@ -1,5 +1,13 @@
 from DataProcess import *
+
 imgData = imgDataProcess()
 labelData = labelDataProcess()
-combineSeq(imgData, labelData)  # 生成'pdData.csv'
+combineSeq(imgData, labelData)
+
+# 生成'pdData.csv'
 Data = pd.read_csv('pdData.csv')
+
+# 生成'pdDataNF.csv'，该文件是将funny标签转为smiling
+Data.loc[Data['Face'] == "funny"] = "smiling"
+print(list(set(Data['Face'])))
+Data.to_csv('pdDataNF.csv', encoding='utf-8')
