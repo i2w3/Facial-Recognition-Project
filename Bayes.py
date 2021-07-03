960c6f1dcdb647e0452698972351da858bd5f3a9
Nya2233 <45359144+i2w3@users.noreply.github.com>
2021-07-04 00:09:30 +0800

    Fix pdDataNF.csv & Add Naive Bayes test pdDataNF.csv

--- Bayes.py
+++ Bayes.py
@@ -32,11 +32,29 @@ for name, data in (["Raw", X], ["PCA", X1], ["LBP", X2], ["LBP + PCA", X3]):
     msg = "%s NB: %f (%f)" % (name, cv_results.mean(), cv_results.std())
     print(msg)
 
-'''
-print("Bernoulli Bayes：")
-for name, data in (["Raw", X], ["PCA", X1], ["LBP", X2], ["LBP+PCA", X3]):
+DataNF = pd.read_csv('pdDataNF.csv')
+X = combineSeqData(DataNF)
+y = np.array(DataNF['Face'].values)
+
+# X1 27个特征
+pca = PCA(n_components=27, svd_solver='auto',
+          whiten=True).fit(X)
+X1 = pca.transform(X)
+
+# X2 LBP
+X2 = combineLBPSeqData(DataNF)
+
+# X3 LBP + PCA 27个特征
+pca = PCA(n_components=27, svd_solver='auto',
+          whiten=True).fit(X2)
+X3 = pca.transform(X2)
+
+num_folds = 10
+scoring = 'accuracy'
+
+print("Combined funny&smiling Naive Bayes：")
+for name, data in (["Raw", X], ["PCA", X1], ["LBP", X2], ["LBP + PCA", X3]):
     kfold = model_selection.KFold(n_splits=num_folds)
-    cv_results = model_selection.cross_val_score(BernoulliNB(), data, y, cv=kfold, scoring=scoring)
+    cv_results = model_selection.cross_val_score(GaussianNB(), data, y, cv=kfold, scoring=scoring)
     msg = "%s NB: %f (%f)" % (name, cv_results.mean(), cv_results.std())
     print(msg)
-'''
--- build_pdData.py
+++ build_pdData.py
@@ -5,9 +5,11 @@ labelData = labelDataProcess()
 combineSeq(imgData, labelData)
 
 # 生成'pdData.csv'
-Data = pd.read_csv('pdData.csv')
+Data = pd.read_csv('pdData.csv', index_col=0)
+print(list(set(Data['Face'])))
 
 # 生成'pdDataNF.csv'，该文件是将funny标签转为smiling
-Data.loc[Data['Face'] == "funny"] = "smiling"
+Data["Face"].replace("funny", "smiling", inplace=True)
 print(list(set(Data['Face'])))
 Data.to_csv('pdDataNF.csv', encoding='utf-8')
+DataNF = pd.read_csv('pdDataNF.csv')
